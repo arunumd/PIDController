d813d67573161904ec77f7a1e8641140546a5803
Arun Kumar D <arun89@terpmail.umd.edu>
2018-09-22 21:35:33 -0400

    test.cpp file was recoded. Shared pointer was giving issues. Hence we removed shared pointers and made normal implementation.

--- test/test.cpp
+++ test/test.cpp
@@ -5,19 +5,19 @@
 */
 
 #include <gtest/gtest.h>
-#include <memory>
 #include <PID_Controller.hpp>
 
 
 /*This test checks whether the two values are double*/
 TEST(PID_Controller, zeroError) {
-	std::shared_ptr<PidController> testController = std::make_shared<PidController>(); // Creates an instance of pidController
-	EXPECT_DOUBLE_EQ(0, testController->calculateGain(65,10));//compares two double to expect them to be equal. calls calculate gain with a speed of 65 and a time of 10
+	PidController testController(0.1,0.1,0.1,60,0,65);
+	EXPECT_DOUBLE_EQ(0, testController.calculateGain(65,10));//compares two double to expect them to be equal. calls calculate gain with a speed of 65 and a time of 10
 }
 
 /*This test checks for the sign of the gains (positive or negative)*/
-TEST(PID_Controller, positiveGain){
-	std::shared_ptr<PidController> testController = std::make_shared<PidController>(); // Creates an instance of pidController
-	EXPECT_TRUE(0<testController->calculateGain(80,10)); // Checks the sign of the gain
-	EXPECT_TRUE(0>testController->calculateGain(50,10)); // Checks the sign of the gain.
+TEST(PID_Controller, checkSign){
+	PidController testController(0.1,0.1,0.1,60,0,65);
+	EXPECT_TRUE(0<testController.calculateGain(80,10)); // Checks the sign of the gain
+	EXPECT_TRUE(0>testController.calculateGain(50,10)); // Checks the sign of the gain.
 }
+
