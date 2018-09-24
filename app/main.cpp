878b6822cb200699fba641841df3ab351cecd52f
Niket <s.niket28@gmail.com>
2018-09-24 12:51:26 -0400

    app/main: Commented out the usage of set methods

--- app/main.cpp
+++ app/main.cpp
@@ -11,13 +11,13 @@ int main() {
   /*Instantiation of PIDController type object*/
   PidController PID1(5.0, 5.0, 5.0, 500.0, 17.0, 675.0);
 
-  PID1.setKp(5.0); /*The args should be kp value*/
+  /*PID1.setKp(5.0); // The args should be kp value
 
-  PID1.setKi(5.0); /*The args should be ki value*/
+   PID1.setKi(5.0); // The args should be ki value
 
-  PID1.setKd(5.0); /*The args should be kd value*/
+   PID1.setKd(5.0); // The args should be kd value
 
-  PID1.setSetPoint(675.0); /*The args should be setpoint value*/
+   PID1.setSetPoint(675.0); // The args should be setpoint value */
 
   /*Calling function to calculate gain for the current speed and time*/
   double gain = PID1.calculateGain(500.0, 17);
